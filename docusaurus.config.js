e3ed17e4a6e6b4d6e03b63fb503b4a47135bb77d
Jeff Terrell <jeffaterrell@gmail.com>
2022-03-29 17:45:59 -0400

    add initial project files and folders

---
+++ docusaurus.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Low code Python web framework',
+  tagline: 'Low code Python web framework',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'pywebio', // Usually your GitHub org/user name.
+  projectName: 'pywebio', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'PyWebIO',
+        logo: {
+          alt: 'PyWebIO logo',
+          src: 'img/pywebio.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/pywebio/PyWebIO/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/MvaCcg76Z7',
+              },
+              {
+                label: 'Subscribe to newsletter',
+                href: 'https://TODO',
+              },
+              {
+                label: 'Contact us',
+                href: 'https://TODO',
+              },
+            ],
+          },
+          {
+            title: 'Products and Services',
+            items: [
+              {
+                label: 'Library and collateral',
+                to: 'https://TODO',
+              },
+              {
+                label: 'Platform',
+                href: 'https://TODO',
+              },
+              {
+                label: 'Hire an expert',
+                href: 'https://TODO'
+              }
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
