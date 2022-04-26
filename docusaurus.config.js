af865a6369a13fb2522ec98050e1785e2c8475e2
Jeff Terrell <jeffaterrell@gmail.com>
2022-04-26 12:03:42 -0400

    add Google Analytics trackingID

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-L82K5WY36H',
+          anonymizeIP: true,
+        }
       }),
     ],
   ],
